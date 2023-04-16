d3a367199e1a4c671771380c039ed0591b97ea21
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2023-04-16 12:53:31 -0300

    Create 1049.py

---
+++ Lista 05/1049.py
@@ -0,0 +1,19 @@
+p1 = str(input())
+p2 = str(input())
+p3 = str(input())
+if p1 == 'vertebrado' and p2 == 'ave' and p3 == 'carnivoro':
+  print("aguia")
+elif p1 == 'vertebrado' and p2 == 'ave' and p3 == 'onivoro':
+  print("pomba")
+elif p1 == 'vertebrado' and p2 == 'mamifero' and p3 == 'onivoro':
+  print("homem")
+elif p1 == 'vertebrado' and p2 == 'mamifero' and p3 == 'herbivoro':
+  print("vaca")
+elif p1 == 'invertebrado' and p2 == 'inseto' and p3 == 'hematofago':
+  print("pulga")
+elif p1 == 'invertebrado' and p2 == 'inseto' and p3 == 'herbivoro':
+  print("lagarta")
+elif p1 == 'invertebrado' and p2 == 'anelideo' and p3 == 'hematofago':
+  print("sanguessuga")
+elif p1 == 'invertebrado' and p2 == 'anelideo' and p3 == 'onivoro':
+  print("minhoca")
