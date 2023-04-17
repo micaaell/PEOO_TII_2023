11ce642b4252eab3afa1430b7e237c0639033a8e
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2023-04-17 11:08:42 -0300

    Create 1012.py

---
+++ Lista 04/1012.py
@@ -0,0 +1,10 @@
+A,B,C,=map(float, input().split()) 
+A= float(A)
+B= float(B)
+C= float(C)
+print(f"TRIANGULO: {(A*C) / 2.00:.3f}")
+pi=3.14159
+print (f"CIRCULO: {pi*C*C:.3f}")
+print(f"TRAPEZIO: {(A+B)* C/ 2.0:.3f}")
+print(f"QUADRADO: { B*B:.3f}")
+print(f"RETANGULO: { A*B:.3f}")
