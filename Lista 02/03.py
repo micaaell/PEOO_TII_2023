f69b25c06dab195cbd26a85adbdace904bdb5bea
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2023-04-23 23:25:58 -0300

    Create 03.py

---
+++ Lista 02/03.py
@@ -0,0 +1,6 @@
+print("Digite a nota do primeiro bimestre da disciplina:")
+n1=int(input())
+print("Digite a nota do segundo bimestre da disciplina:")
+n2=int(input())
+soma=((n1*2)+(n2*3))/5
+print(f"Média parcial = {soma}")
