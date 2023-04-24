d2f3289a79213ee239111c4ca9c6f3580607fb83
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2023-04-24 11:53:32 -0300

    Create 04.py

---
+++ Lista 02/04.py
@@ -0,0 +1,7 @@
+print("Digite a base e a altura do retângulo:")
+b=int(input())
+h=int(input())
+area=b*h
+peri=(b*2)+(h*2)
+diagonal=((b**2)+(h**2))**(1/2)
+print(f"Área = {area} - Perímetro = {peri}- Diagonal = {diagonal}")
