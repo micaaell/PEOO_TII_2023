41ac474856b845e76fa190713e9147125b2d2df0
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2023-04-28 10:00:39 -0300

    Create 03.py

---
+++ RevLista01/03.py
@@ -0,0 +1,25 @@
+print("Digite quatro valores inteiros:")
+n1=int(input())
+n2=int(input())
+n3=int(input())
+n4=int(input())
+par=0
+impar=0
+if n1%2==0:
+	par=n1
+else:
+	impar=n1
+if n2%2==0:
+	par+=n2
+else:
+	impar+=n2
+if n3%2==0:
+	par+=n3
+else:
+	impar+=n3
+if n4%2==0:
+	par+=n4
+else:
+	impar+=n4
+print(f"Soma dos pares = {par}")
+print(f"Soma dos pares = {impar}")
