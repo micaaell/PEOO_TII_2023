f52962f962934380f524df7b9c2a67e362994086
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2023-05-04 23:27:33 -0300

    Create 05.py

---
+++ RevLista01/05.py
@@ -0,0 +1,26 @@
+print("Informe o número do mês:")
+m=int(input())
+if m==1:
+	print("O mês de janeiro é do primeiro trimestre do ano")
+if m==2:
+	print("O mês de Fevereiro é do primeiro trimestre do ano")
+if m==3:
+	print("O mês de Março é do primeiro trimestre do ano")
+if m==4:
+	print("O mês de Abril é do segundo trimestre do ano")
+if m==5:
+	print("O mês de Maio é do segundo trimestre do ano")
+if m==6:
+	print("O mês de Junho é do segundo trimestre do ano")
+if m==7:
+	print("O mês de Julho é doTerceiro trimestre do ano")
+if m==8:
+	print("O mês de Agosto é do Terceiro trimestre do ano")
+if m==9:
+	print("O mês de Setembro é do terceiro trimestre do ano")
+if m==10:
+	print("O mês de Outubro é do quarto trimestre do ano")
+if m==11:
+	print("O mês de Novembro é do quarto trimestre do ano")
+if m==12:
+	print("O mês de Dezembro é do quarto trimestre do ano")
