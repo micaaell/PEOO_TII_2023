5cf4205b68f8bb96d5615b730142d9ecfa68a4f7
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2023-10-29 22:42:27 -0300

    Create index.py

---
+++ Streamlit/Agenda/index.py
@@ -0,0 +1,29 @@
+from templates.manterclienteUI import ManterClienteUI
+from templates.manterlogin import Manterlogin
+from templates.manterservicoUI import ManterServicoUI
+from templates.manteragendaUI import ManterAgendaUI
+from templates.abriragendaUI import AbrirAgendaUI
+
+import streamlit as st
+
+class IndexUI:
+      
+    def sidebar():
+      op = st.sidebar.selectbox("Menu", ["Manter Clientes", "Manter Serviços", "Manter Agenda", "Abrir Agenda do Dia","Cadrastre-se"])
+      if op == "Manter Clientes": ManterClienteUI.main()
+      if op == "Manter Serviços": ManterServicoUI.main()
+      if op == "Manter Agenda": ManterAgendaUI.main()
+      if op == "Abrir Agenda do Dia": AbrirAgendaUI.main()
+      if op == "Cadrastre-se": Manterlogin.main()
+
+      #if op == "Manter Clientes": st.session_state["page"] = "manter_clienteUI"
+
+    def main():
+      IndexUI.sidebar()
+
+      #if "page" not in st.session_state: st.session_state["page"] = "equacaoUI"
+      #if st.session_state["page"] == "manter_clienteUI": ManterClienteUI.main()
+
+IndexUI.main()
+
+
